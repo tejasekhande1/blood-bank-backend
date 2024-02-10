666c5c90089a6876bd65443cbc45800f8a367968
tejasekhande1 <ekhandetejas741@gmail.com>
2024-02-11 01:28:08 +0530

    add getUserProfile details controller

--- controllers/User.js
+++ controllers/User.js
@@ -154,8 +154,6 @@ exports.updateUserAvailability = async (req, res) => {
             });
         }
 
-        console.log("Profile -> ", userProfile);
-        console.log("User Availability -> ", userProfile.availability);
         return res.status(200).json({
             success: true,
             message: `Availability updated successfully`,
@@ -166,6 +164,30 @@ exports.updateUserAvailability = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: 'Failed to update user availability',
+            error: error.message
+        });
+    }
+}
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).populate('profile')
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: `User profile details fetched`,
+            data: user
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to get user profile',
             error: error.message
         });
     }
--- routes/User.js
+++ routes/User.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 
 const { auth } = require('../middlewares/Auth');
-const { updateProfileDetails, addNotification, getNotifications, updateNotification, updateUserAvailability } = require("../controllers/User");
+const { updateProfileDetails, addNotification, getNotifications, updateNotification, updateUserAvailability, getUserProfile } = require("../controllers/User");
 
 router.put('/profile', auth, updateProfileDetails);
+router.get('/profile', auth, getUserProfile)
 router.post('/notification', auth, addNotification);
 router.get('/notification', auth, getNotifications);
 router.patch('/notification/:notificationId', auth, updateNotification);
